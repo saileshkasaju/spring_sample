47f228cf386b70bd4d07a4b59a64d9a832a66325
saileshkasaju <leosailesh@gmail.com>
2017-04-27 23:05:47 +0545

    autowired by annotation

--- src/main/java/com/pluralsight/Application.java
+++ src/main/java/com/pluralsight/Application.java
@@ -2,13 +2,17 @@ package com.pluralsight;
 
 import com.pluralsight.service.CustomerService;
 import com.pluralsight.service.CustomerServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Created by Edge on 12/26/2016.
  */
 public class Application {
     public static void main(String[] args) {
-        CustomerService service = new CustomerServiceImpl();
+
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        CustomerService service = applicationContext.getBean("customerService", CustomerService.class);
         System.out.println(service.findAll().get(0).getFirstname());
     }
 }
