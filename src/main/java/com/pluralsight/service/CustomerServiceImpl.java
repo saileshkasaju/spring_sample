50b92258c90cac7bef04834abbe24c1534713beb
saileshkasaju <leosailesh@gmail.com>
2017-04-27 23:08:26 +0545

    change to using Autowired setter injection

--- src/main/java/com/pluralsight/service/CustomerServiceImpl.java
+++ src/main/java/com/pluralsight/service/CustomerServiceImpl.java
@@ -12,8 +12,14 @@ import java.util.List;
  */
 @Service("customerService")
 public class CustomerServiceImpl implements CustomerService {
-    @Autowired
+    //@Autowired
     private CustomerRepository customerRepository;
+
+    @Autowired
+    public void setCustomerRepository(CustomerRepository customerRepository) {
+        System.out.println("We are using setter injection");
+        this.customerRepository = customerRepository;
+    }
 
     @Override
     public List<Customer> findAll() {
