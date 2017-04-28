1afe018b6c5c7a7f39cb3cd736664d068f816b08
saileshkasaju <leosailesh@gmail.com>
2017-04-28 07:54:52 +0545

    create constructor snd use constructor injection

--- src/main/java/com/pluralsight/service/CustomerServiceImpl.java
+++ src/main/java/com/pluralsight/service/CustomerServiceImpl.java
@@ -12,6 +12,9 @@ public class CustomerServiceImpl implements CustomerService {
 
     private CustomerRepository customerRepository;
 
+    public CustomerServiceImpl(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
 
     public void setCustomerRepository(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
