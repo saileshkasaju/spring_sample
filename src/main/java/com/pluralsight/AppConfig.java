1fe02989f118d111b0a2de049549e1d893e1a9d6
saileshkasaju <leosailesh@gmail.com>
2017-04-28 07:59:31 +0545

    add componentScan for autowiring

--- src/main/java/com/pluralsight/AppConfig.java
+++ src/main/java/com/pluralsight/AppConfig.java
@@ -5,12 +5,14 @@ import com.pluralsight.repository.HibernateCustomerRepositoryImpl;
 import com.pluralsight.service.CustomerService;
 import com.pluralsight.service.CustomerServiceImpl;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by Edge on 4/28/2017.
  */
 @Configuration
+@ComponentScan({"com.pluralsight"})
 public class AppConfig {
 
     @Bean(name = "customerService")
